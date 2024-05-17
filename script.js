656e2dcf0e26e1c74e5375923f264a075033d042
Antony K Nayakam <76090437+knantony@users.noreply.github.com>
2024-05-18 01:21:28 +0530

    day2

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 function toggleMenu() {
-const menu = document.querySelector(".menu-links");
-const icon = document.querySelector(".hamburger-icon");
-menu.classList.toggle("open");
-menu.classList.toggle("open");
-}
+    const menu = document.querySelector(".menu-links");
+    menu.classList.toggle("open");
+  }
